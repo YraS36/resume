c8daddb926cc4aebeae28c2f31767b704366bbbb
YraS36 <kokermoy@gmail.com>
2023-03-26 16:44:19 +0300

    add shophome

--- src/route/index.js
+++ src/route/index.js
@@ -1132,7 +1132,64 @@ router.get('/task21', function (req, res) {
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
+// ================================================================
+router.get('/task22', function (req, res) {
+  // res.render генерує нам HTML сторінку
 
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('task22', {
+    layout: 'basic',
+    page_header: {
+      title: 'InnovaMind',
+      description: 'Welcome to our wonderful community',
+    },
+    section: {
+      about_us: {
+        title_list: {
+          main: 'InnovaMind',
+          sub: 'InnovaMind is a forward-thinking startup focused on innovation. We are dedicated to creating efficient and powerful software through the use of first-class functions.',
+        },
+        button: { text: 'Show more' },
+      },
+      advantage: {
+        title: 'Advantage',
+        efficiency: {
+          title: 'Efficiency',
+          sub_title: 'Streamline your software development',
+          description:
+            'Streamline your software development with our innovative platform...',
+          button: 'Show more',
+        },
+        innovation: {
+          title: 'Innovation',
+          sub_title:
+            'Forward-thinking approach to programming',
+          description:
+            ' Stay ahead of the curve with our forward-thinking approach to programming...',
+          button: 'Show more',
+        },
+        collaboration: {
+          title: 'Collaboration',
+          sub_title: 'Connect with like-minded developers',
+          description:
+            'Connect with a community of like-minded developers to share knowledge and insights...',
+          button: 'Show more',
+        },
+      },
+      agreement: {
+        title: 'Terms of use',
+        info: {
+          sub_title: 'Check out our requirements',
+          detail: {
+            text: 'By using or accessing the service, you agree to be bound by the terms and conditions outlined in the agreement.',
+            button: 'Аccept',
+          },
+        },
+      },
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 // ================================================================
 
 router.get('/shophome', function (req, res) {
@@ -1415,193 +1472,18 @@ router.get('/shophome', function (req, res) {
         },
       ],
     ],
-      button: {
-        text: 'Lets start',
-        href: 'https://nodejs.org/en/',
-      },
-    },
-    header: {
-      title: 'Welcome to Our Website',
-      description:
-        'We offer innovative solutions to help grow your business. Our team of experts is committed to providing tailored strategies that deliver the best results.',
-      button: {
-        text: 'Learn More',
-        link: 'https://www.youtube.com/',
-      },
-    },
-    city: {
-      title: 'City list',
-      cards: [
-        {
-          picture: {
-            src: 'https://picsum.photos/200/100',
-            alt: 'New York',
-          },
-          title: 'New York City',
-          description:
-            'New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.',
-          button: {
-            text: 'Read',
-            href: 'https://github.com/',
-          },
-        },
-        {
-          picture: {
-            src: 'https://picsum.photos/200/100',
-            alt: 'Paris',
-          },
-          title: 'Paris',
-          description:
-            "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy, and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture, haute couture fashion houses, and designer boutiques along the Rue du Faubourg Saint-Honoré.",
-          button: {
-            text: 'Read',
-            href: 'https://github.com/',
-          },
-        },
-        {
-          picture: {
-            src: 'https://picsum.photos/200/100',
-            alt: 'Tokyo',
-          },
-          title: 'Tokyo',
-          description:
-            "Tokyo, Japan’s bustling capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) to a reconstructed kabuki theater (in the Edo-Tokyo Museum).",
-          button: {
-            text: 'Reac',
-            href: 'https://github.com/',
-          },
-        },
-        {
-          picture: {
-            src: 'https://picsum.photos/200/100',
-            alt: 'Sydney',
-          },
-          title: 'Sydney',
-          description:
-            "Sydney, capital of New South Wales and one of Australia's largest cities, is best known for its harborfront Opera House, with a distinctive sail-like design. Massive Darling Harbour and the smaller Circular Quay port are hubs of waterside life, with the arched Harbour Bridge and esteemed Royal Botanic Garden nearby. Sydney Tower’s outdoor platform, the Skywalk, offers 360-degree views of the city and suburbs.",
-          button: {
-            text: 'Reac',
-            href: 'https://github.com/',
-          },
-        },
-      ],
-    },
-    winner: {
-      title: 'Our winners',
-      table: {
-        head: ['#', 'name', 'surname', 'nickname'],
-        user: [
-          {
-            place: 1,
-            name: 'Ada',
-            surname: 'Lovelace',
-            nickname: 'The Enchantress of Numbers',
-          },
-          {
-            place: 2,
-            name: 'Grace',
-            surname: 'Hopper',
-            nickname: 'Amazing Grace',
-          },
-          {
-            place: 3,
-            name: 'Katherine',
-            surname: 'Johnson',
-            nickname: 'The Human Computer',
-          },
-        ],
-      },
-    },
-    gallery: {
-      title: 'Picture history',
-      cards: [
-        {
-          name: 'The Mona Lisa',
-          items: [
-            'Painting',
-            'Oil on poplar panel',
-            '1503-1519',
-          ],
-        },
-        {
-          name: 'The Persistence of Memory',
-          items: ['Painting', 'Oil on canvas', '1931'],
-        },
-        {
-          name: 'The Starry Night',
-          items: ['Painting', 'Oil on canvas', '1889'],
-        },
-      ],
-    },
-    relaxion: {
-      title: 'Relax',
-      cards: [
-        {
-          header: 'Meditation',
-          title: 'The Benefits of Meditation',
-          description:
-            'Meditation can help reduce stress, anxiety, and depression, and improve overall well-being. Studies have also shown that regular meditation practice can enhance cognitive function and even boost the immune system.',
-        },
-        {
-          header: 'Hydration',
-          title: 'The Importance of Hydration',
-          description:
-            'Drinking enough water is essential for good health. Dehydration can lead to a range of symptoms, including fatigue, headaches, and constipation. It can also increase the risk of kidney stones and other health problems.',
-        },
-        {
-          header: 'Yoga',
-          title: 'The Benefits of Yoga',
-          description:
-            'Yoga can improve flexibility, strength, and balance, and help reduce stress and anxiety. It has also been shown to improve heart health and promote a sense of well-being.',
-        },
-        {
-          header: 'Sunscreen',
-          title: 'The Importance of Sunscreen',
-          description:
-            "Protecting your skin from the sun's harmful rays is crucial for reducing the risk of skin cancer and premature aging. Experts recommend using a broad-spectrum sunscreen with an SPF of at least 30 and reapplying every two hours.",
-        },
-      ],
-    },
-    rest: {
-      cards: [
-        {
-          header: 'Cooking',
-          title: 'The Joy of Cooking',
-          description:
-            'Cooking is a creative outlet that can bring people together over delicious food.',
-          button: 'Learn More',
-          footer: 'Photo by Brooke Lark on Unsplash',
-        },
-        {
-          header: 'Travel',
-          title: 'The Benefits of Traveling',
-          description:
-            'Traveling broadens your perspective, provides new experiences, and creates lasting memories.',
-          button: 'Explore',
-          footer: 'Photo by Sime Basioli on Unsplash',
-        },
-      ],
-    },
-    proposition: {
-      title: 'Proposition',
-      card: [
-        {
-          header: 'Our Approach',
-          title: 'Innovative Solutions for Your Business',
-          description:
-            'We create effective solutions that cater to your business needs.',
-        },
-        {
-          header: 'Our Expertise',
-          title: 'Trusted and Experienced Professionals',
-          description:
-            'Our team of experts delivers tailored strategies for best results.',
-        },
-      ],
-    },
-    footer: {
-      title: 'Navigation',
-      navigation: [
+  })
+})
+
+// ================================================================
+router.get('/shopproduct', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('shopproduct', {
+    layout: 'shop',
+    navigation: {
+      links: [
         {
           text: 'Home',
           href: 'https://github.com/',
@@ -1615,35 +1497,195 @@ router.get('/shophome', function (req, res) {
           href: 'https://www.youtube.com/',
         },
       ],
-      socialMedia: [
-        {
-          name: 'Facebook',
-          link: 'https://www.facebook.com/',
-        },
-        {
-          name: 'Twitter',
-          link: 'https://twitter.com/',
-        },
-        {
-          name: 'Instagram',
-          link: 'https://www.instagram.com/',
-        },
+      Logo: 'Logo',
+    },
+
+    breadcrumb: [
+      { name: 'Home', url: 'https://github.com/' },
+      { name: 'PC', url: 'https://www.google.com/' },
+      { name: 'Windows', url: 'https://github.com/' },
+      { name: 'Product PC #3123', url: null },
+    ],
+
+    productData: {
+      img: 'https://picsum.photos/400/200',
+
+      info: {
+        title: 'Product PC Asus 331 BC 671',
+        about:
+          "Some quick example text to build on the card title and make up the bulk of the card's content.",
+        price: '1000$',
+
+        category: [
+          { name: 'HOT', isHot: true },
+          { name: 'New', isNew: true },
+          { name: 'Bonus', isBonus: true },
+        ],
+      },
+
+      actionList: [
+        { name: 'Buy', isWarning: true },
+        { name: 'Add to cart', isError: true },
+        { name: 'Like', isSuccess: true },
+        { name: 'Share', isGood: true },
       ],
-      partners: [
+
+      item: {
+        head: ['#', 'color', 'price'],
+
+        body: [
+          ['id312', 'red', '1100$'],
+          ['id532', 'blue', '1050$'],
+          ['id643', 'dark', '999$'],
+        ],
+      },
+
+      param: {
+        title: 'Product params',
+        list: ['Экран 15.6', 'IPS (1920x1080)', 'Full HD'],
+      },
+
+      alert: {
+        isError: true,
+        text: 'Danger! This is a danger alert — check it out!',
+      },
+    },
+
+    goodsOtherBlock: {
+      title: 'See other products',
+      cards: [
         {
-          name: 'Acme Inc.',
-          link: 'https://www.acme.com/',
+          image: 'https://picsum.photos/400/200',
+          title: 'iPhone 13',
+          description:
+            'The latest iPhone model features a new A15 Bionic chip, improved camera system, and longer battery life.',
+          isHot: false,
+          idNew: false,
         },
         {
-          name: 'Globex Corporation',
-          link: 'https://www.globex.com/',
+          image: 'https://picsum.photos/400/200',
+          title: 'MacBook Pro',
+          description:
+            "Apple's high-end laptop features a 16-inch Retina display, powerful M1 Pro or M1 Max chip, and up to 64GB of RAM.",
+          isHot: true,
+          idNew: false,
         },
         {
-          name: 'Stark Industries',
-          link: 'https://www.starkindustries.com/',
+          image: 'https://picsum.photos/400/200',
+          title: 'AirPods Pro',
+          description:
+            "Apple's premium wireless earbuds feature active noise cancellation, a customizable fit, and up to 4.5 hours of listening time.",
+          isHot: false,
+          idNew: false,
         },
       ],
     },
+
+    service: {
+      title: 'Our Services',
+      description:
+        'We offer a variety of services to meet your needs, including web design, content creation, and social media management.',
+      buttons: [
+        {
+          text: 'Show More',
+          link: 'https://www.youtube.com/',
+          rel: 'show-more',
+        },
+      ],
+    },
+
+    footer: [
+      [
+        {
+          text: 'Home',
+          link: 'https://www.youtube.com/',
+        },
+        {
+          text: 'Trending',
+          link: 'https://www.youtube.com/feed/trending',
+        },
+        {
+          text: 'Subscriptions',
+          link: 'https://www.youtube.com/feed/subscriptions',
+        },
+        {
+          text: 'Library',
+          link: 'https://www.youtube.com/feed/library',
+        },
+      ],
+      [
+        {
+          text: 'History',
+          link: 'https://www.youtube.com/feed/history',
+        },
+        {
+          text: 'Your Videos',
+          link: 'https://www.youtube.com/feed/my_videos',
+        },
+        {
+          text: 'Live',
+          link: 'https://www.youtube.com/live',
+        },
+        {
+          text: 'Settings',
+          link: 'https://www.youtube.com/account',
+        },
+      ],
+      [
+        {
+          text: 'Watch Later',
+          link: 'https://www.youtube.com/playlist?list=WL',
+        },
+        {
+          text: 'Liked Videos',
+          link: 'https://www.youtube.com/playlist?list=LL',
+        },
+        {
+          text: 'Music',
+          link: 'https://www.youtube.com/music',
+        },
+        {
+          text: 'Gaming',
+          link: 'https://www.youtube.com/gaming',
+        },
+      ],
+      [
+        {
+          text: 'Sports',
+          link: 'https://www.youtube.com/channel/UCo_q6aOlvPH7M-j_XGWVgXg',
+        },
+        {
+          text: 'News',
+          link: 'https://www.youtube.com/news',
+        },
+        {
+          text: 'Fashion & Beauty',
+          link: 'https://www.youtube.com/channel/UC1x8rV_f-2yPpzlN0JWZXIQ',
+        },
+        {
+          text: 'Learning',
+          link: 'https://www.youtube.com/learning',
+        },
+      ],
+      [
+        {
+          text: 'Report History',
+          link: 'https://www.youtube.com/feed/history/report_history',
+        },
+        {
+          text: 'Help',
+          link: 'https://support.google.com/youtube/?hl=en',
+        },
+        {
+          text: 'Send Feedback',
+          link: 'https://support.google.com/youtube/answer/4347644?hl=en',
+        },
+        {
+          text: 'About',
+          link: 'https://www.youtube.com/about/',
+        },
+      ],
+    ],
   })
   //                  ↑↑ сюди вводимо JSON дані
 })
